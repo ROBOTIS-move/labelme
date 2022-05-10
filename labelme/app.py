43f42998ae4bd603a7a9ee1a92e596f88174f41c
eungiCho <ceg@robotis.com>
2022-05-10 10:52:01 +0900

    Modified button names
    Signed-off-by: eungiCho <ceg@robotis.com>

--- labelme/app.py
+++ labelme/app.py
@@ -434,14 +434,14 @@ class MainWindow(QtWidgets.QMainWindow):
         )
 
         hideAll = action(
-            self.tr("&Hide\nPolygons"),
+            self.tr("&Hide\nAll"),
             functools.partial(self.togglePolygons, False),
             icon="eye",
             tip=self.tr("Hide all polygons"),
             enabled=False,
         )
         showAll = action(
-            self.tr("&Show\nPolygons"),
+            self.tr("&Show\nAll"),
             functools.partial(self.togglePolygons, True),
             icon="eye",
             tip=self.tr("Show all polygons"),
