ae920c5c27f99a82a9a661c6852e9158d8ca949c
eungiCho <ceg@robotis.com>
2022-06-24 16:49:32 +0900

    Modified auto save action
    Signed-off-by: eungiCho <ceg@robotis.com>

--- labelme/app.py
+++ labelme/app.py
@@ -283,9 +283,9 @@ class MainWindow(QtWidgets.QMainWindow):
         )
 
         saveAuto = action(
-            text=self.tr("Save &Automatically"),
-            slot=lambda x: self.actions.saveAuto.setChecked(x),
-            icon="save",
+            self.tr("Save &Automatically"),
+            self.toggleAutoSaveMode,
+            icon=None,
             tip=self.tr("Save automatically"),
             checkable=True,
             enabled=True,
@@ -326,7 +326,7 @@ class MainWindow(QtWidgets.QMainWindow):
             self.tr('Toggle "add point to edge" mode'),
             checkable=True,
         )
-        add_point_mode.setChecked(self._config["keep_prev"])
+        add_point_mode.setChecked(self._config["add_point"])
 
         createMode = action(
             self.tr("Create Polygons"),
@@ -1980,6 +1980,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def toggleKeepPrevMode(self):
         self._config["keep_prev"] = not self._config["keep_prev"]
+
+    def toggleAutoSaveMode(self):
+        self._config["auto_save"] = not self._config["auto_save"]
 
     def toggleAddPointMode(self):
         self._config["add_point"] = not self._config["add_point"]
