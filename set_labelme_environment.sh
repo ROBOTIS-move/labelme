ff92db935c1b083710670060dbee29b5048bdec2
eungiCho <ceg@robotis.com>
2022-11-01 16:31:33 +0900

    Modified setting script
    Signed-off-by: eungiCho <ceg@robotis.com>

--- set_labelme_environment.sh
+++ set_labelme_environment.sh
@@ -2,7 +2,34 @@
 
 sudo apt-get install -y curl
 
-curl -L https://github.com/wkentaro/dotfiles/raw/main/local/bin/install_anaconda3.sh | bash -s .
+INSTALL_DIR=$(pwd)
+
+if [ -e $INSTALL_DIR/.anaconda3 ]; then
+  echo "Anaconda3 is already installed: $INSTALL_DIR/.anaconda3"
+  exit 0
+fi
+
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+
+if [ "$(uname)" = "Linux" ]; then
+  wget -q 'https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh' -O miniconda2.sh
+elif [ "$(uname)" = "Darwin" ]; then
+  wget -q 'https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh' -O miniconda2.sh
+else
+  echo "[$(basename $0)] Unsupported platform: $(uname)"
+  exit 0
+fi
+
+bash ./miniconda2.sh -p $INSTALL_DIR/.anaconda3 -b
+cd -
+rm -rf $TMPDIR
+
+source $INSTALL_DIR/.anaconda3/bin/activate
+conda update -n base -y conda
+conda install -n base -y pip  # pip is uninstalled when conda is updated
+source $INSTALL_DIR/.anaconda3/bin/deactivate
+
 source .anaconda3/bin/activate
 pip install -e .
 
