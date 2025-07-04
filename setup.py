7de80b427ecf49c0a1ea7269258ea0b81247c354
Hun Jung <jsh@robotis.com>
2025-07-04 10:06:20 +0900

    Added require pkg
    Signed-off-by: Hun Jung <jsh@robotis.com>

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ def get_install_requires():
         "termcolor",
         "cryptography",
         "lxml",
-        "requests"
+        "requests",
+        "pip install opencv-python==4.11.0.86"
     ]
 
     # Find python binding for qt with priority:
